fa3b4a74f43df4a4b0cbc69be76fa8aaa2e15e01
Oksana_P <popovtseva.o.n@yandex.ru>
2022-10-21 13:22:39 +0500

    Add homework for seminar 6

---
+++ Homework_006/Task_041/Program.cs
@@ -0,0 +1,72 @@
+﻿// 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//Программа
+Console.Clear();
+string[] myNumbers = GetMessage("Введите числа через запятую");
+
+if (Array.TrueForAll(myNumbers, IsNumbersValid))
+{
+    int[] MyNums = GetNumbersInt(myNumbers);
+    PrintArray(MyNums);
+    int countIndex = CountPositive(MyNums);
+    Console.WriteLine();
+    Console.WriteLine("Количество чисел больше нуля = " + countIndex);
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
+
+
+// Метод для считывания строки
+string[] GetMessage(string message)
+{
+    Console.WriteLine("Введите числа через запятую:");
+    string userNumbers = Console.ReadLine();
+    string[] userNums = userNumbers.Split(new char[] { ',', '.', ' ', ';' }, StringSplitOptions.RemoveEmptyEntries);
+    return userNums;
+}
+
+
+//Метод проверки данных на соотвествие int
+bool IsNumbersValid(string values)
+{
+    int s;
+    return int.TryParse(values.Substring(values.Length - 1), out s);
+}
+
+
+// Метод для перевода строки в массив чисел int
+int[] GetNumbersInt(string[] val)
+{
+    int[] aNums = new int[val.Length];
+    for (int i = 0; i < val.Length; i++)
+    {
+        aNums[i] = int.Parse(val[i]);
+    }
+    return aNums;
+}
+
+
+//Метод для вывода массива
+void PrintArray(int[] arr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + " | ");
+    }
+    Console.Write(arr[arr.Length - 1] + " ]");
+}
+
+//Метод для подсчета чисел больше нуля
+int CountPositive(int[] zahlen)
+{
+    int count = 0;
+    for (int i = 0; i < zahlen.Length; i++)
+    {
+        if (zahlen[i] > 0) count++;
+    }
+    return count;
+}
---
+++ Homework_006/Task_043/Program.cs
@@ -0,0 +1,76 @@
+﻿// 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// Выводим значение x из уравнения:
+// k1 * x + b1 = k2 * x + b2
+// x * (k1-k2)= b2 - b1
+// x = (b2 - b1) / (k1 - k2)
+
+
+//Программа
+Console.Clear();
+string num1 = GetValue("Введите первое число:");
+string num2 = GetValue("Введите второе число:");
+string num3 = GetValue("Введите третье число:");
+string num4 = GetValue("Введите четвёртое число:");
+
+bool IsNum1Double = IsNumberDouble(num1);
+bool IsNum2Double = IsNumberDouble(num2);
+bool IsNum3Double = IsNumberDouble(num3);
+bool IsNum4Double = IsNumberDouble(num4);
+
+if(IsNum1Double && IsNum2Double && IsNum3Double && IsNum4Double)
+{
+    double b1 = GetDouble(num1);
+    double b2 = GetDouble(num2);
+    double k1 = GetDouble(num3);
+    double k2 = GetDouble(num4);
+
+    FindPoint(b1,b2,k1,k2);
+}
+else
+{
+    Console.WriteLine("Неверные данные");
+}
+
+
+// Метод для считывания строки
+string GetValue(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    return value;
+}
+
+//Метод для проверки числа double
+bool IsNumberDouble(string valueA)
+{
+    double Exp1;
+    bool IsDoubleNum = double.TryParse(valueA, out Exp1);
+    if (IsDoubleNum)
+        return true;
+    return false;
+}
+
+//Метод для перевода числа в double
+double GetDouble(string val)
+{
+    double n = double.Parse(val);
+    return n;
+}
+
+
+//Метод для вычисления координат
+void FindPoint (double B1, double B2, double K1, double K2)
+{
+    if(K1-K2 == 0)
+    {
+        Console.WriteLine("Ошибка");
+    }
+    else
+    {
+    double x = Math.Round((B2 - B1) / (K1 - K2),2);
+    double y = Math.Round(K1 * x + B1,2);
+    Console.WriteLine($"Координаты точки, в которой прямые пересекутся = ({x}, {y})");
+    }
+}
